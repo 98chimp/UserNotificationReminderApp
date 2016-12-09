d16f8c7f298945709930d22774ae5752b8e6ccba
Shahin Zangenehpour <shahin@98chimp.com>
2016-12-09 14:38:43 -0800

    Notifications are Working!

--- UserNotificationReminderApp/ViewController.swift
+++ UserNotificationReminderApp/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    @IBAction func datePickerDidSelectNewDate(_ sender: UIDatePicker)
+    {
+        let selectedDate = sender.date
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.scheduleNotifications(at: selectedDate)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
