dce18d3b146f7cc6bf78c50699201b8e03c93ee0
Shahin Zangenehpour <shahin@98chimp.com>
2016-12-09 11:55:00 -0800

    Initial Commit

---
+++ UserNotificationReminderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotificationReminderApp
+//
+//  Created by Shahin on 2016-12-09.
+//  Copyright © 2016 98%Chimp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
